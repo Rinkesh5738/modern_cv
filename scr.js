91e6a798764b65684fcaf55649573deb7903c807
Rinkesh5738 <52352858+Rinkesh5738@users.noreply.github.com>
2022-04-13 11:02:06 +0530

    Update scr.js

--- scr.js
+++ scr.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Ethical Hacker","Pen Tester","Web Developer", "Designer", "Freelancer"],
+        strings: ["Ethical Hacker","Penetration Tester","Web Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Ethical Hacker","Pen Tester","Web Developer", "Designer", "Freelancer"],
+        strings: ["Ethical Hacker","Penetration Tester","Web Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
